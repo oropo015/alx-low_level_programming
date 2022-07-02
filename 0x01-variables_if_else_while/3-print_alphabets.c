2e637247da6d77441374ba9d0edb48e40d60cdc1
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-01 21:07:10 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,9 +14,7 @@ int main(void)
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 		putchar(alpha);
 	for (alpha = 'A'; alpha <= 'Z'; alpha++)
-	{
                 putchar(alpha);
-		putchar('\n');
-	}
+	putchar('\n');
 	return (0);
 }
