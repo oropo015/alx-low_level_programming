ef84f4d2367082ea616d6ae6e49802b070b905da
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-02 12:04:38 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (num != '9')
+		{
+			putchar(num);
+			putchar(',');
+			putchar(' ');
+		}else if (num == '9')
+			putchar(num);
+	}
+	return (0);
+}
