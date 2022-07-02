4e5e65e26f84e2328da251646bc560ef2d125019
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-02 11:46:00 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num);
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
