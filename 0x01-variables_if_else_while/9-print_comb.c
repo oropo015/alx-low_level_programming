d3b2404cca0889b70222a368b933ceceaaca157b
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-02 13:41:29 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,14 +13,13 @@ int main(void)
 
 	for (num = '0'; num <= '9'; num++)
 	{
+		putchar(num);
 		if (num != '9')
 		{
-			putchar(num);
 			putchar(',');
 			putchar(' ');
 		}
-		else if (num == '9')
-			putchar(num);
 	}
+	putchar('\n');
 	return (0);
 }
