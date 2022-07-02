4ae1ec53c6ae3ad0f7cb1f9aa05c1c9666e6cec9
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-01 21:48:15 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num < '10')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
