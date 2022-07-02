d416102f97aae472bc010fd5d02bc9c939919b21
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-02 11:49:05 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,9 @@
 int main(void)
 {
 	char alpha;
-	int num;
+	char num;
 
-	for (num = 0; num < 10; num++)
+	for (num = '0'; num <= '9'; num++)
 		putchar(num);
 	for (alpha = 'a'; alpha <= 'f'; alpha++)
 		putchar(alpha);
