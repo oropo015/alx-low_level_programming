6254538cfff6765a5dffdc36264854b6066a76ea
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-03 13:43:20 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,10 +5,30 @@
  * main -Entry Point
  *
  * Return: Always 0 (Success)
- *
  */
 
- int main(void)
- {
- 	int x;
- }
+int main(void)
+{
+	int i;
+	int j;
+	int x = 1;
+
+	i = 0;
+	while (i < 10)
+	{
+		for (j = x; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
