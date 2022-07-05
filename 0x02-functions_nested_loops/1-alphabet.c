1ab093cfb9bfa07a344796aa9394307511bc85c4
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-05 15:40:41 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
- *
  * print_alphabet - prints all alphabet
+ *
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
