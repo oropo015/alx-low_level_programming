02382227fb69b86a61e6bdab687afb3d0e737224
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-05 15:09:39 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,9 +5,11 @@
  *
  * Return: Always 0	
  */
-
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
 }
