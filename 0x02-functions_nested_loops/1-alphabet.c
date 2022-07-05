8c66d68840013901a0c15766aa184749febee74f
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-05 11:48:46 -0700

    Updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
