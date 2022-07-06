218e716b35ccbdd1e1d99628cd05f264f3bc4f41
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-06 14:07:05 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *_isalpha - check the code for lower case.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c >= 'Z'))
+		return (1);
+	else
+		return (0);
+}
