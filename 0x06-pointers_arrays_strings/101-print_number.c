1dd65efc14001cd1d1c477f04e7a9b6c63b37b05
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-15 21:09:27 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,6 +7,20 @@
  */
 void print_number(int n)
 {
-	int i, a;
-	return (n);
+	int j = n, a = 1, count, m;
+	
+	for (count = 0; j != 0; count++)
+		j = j / 10;
+	while (count > 1)
+	{
+		a = a * 10;
+		count --;
+	}
+	while (n != 0)
+	{
+		m = n / a;
+		n = n % a;
+		a = a / 10;
+		_putchar(m + '0');
+	}
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 int _putchar(char c);
+void print_number(int n);
