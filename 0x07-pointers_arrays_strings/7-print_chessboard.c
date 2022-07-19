aa5feb29a0dc27177c11b687d371aca452144d58
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-19 12:01:37 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - function to print a chess board
+ * @: input
+ * Return: 0 
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+}
