b9b197f01d35c9508f9a302ed84c83edec93b7e5
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-22 15:23:52 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	}
+	printf("%d\n", i - 1);
+	return (0);
+}
