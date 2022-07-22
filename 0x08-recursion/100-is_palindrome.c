e79d2f13e52e7b2281ed0146e57a3aac060ce310
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-07-21 18:19:41 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "main.h"
+int lengthc(char *s);
+int palindrome(char str[], int st, int end);
+/**
+ * is_palindrome - Entry Point
+ * @s: input
+ * Return: 0
+ */
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = lengthc(s);
+
+	if (len == 0)
+		return (1);
+	return (palindrome(s, 0, len - 1));
+}
+
+/**
+ * lengthc - finds the length count
+ * @s: input
+ * Return: length size
+ */
+int lengthc(char *s)
+{
+	if (*s != '\0')
+		return (1 + lengthc(s + 1));
+	return (0);
+}
+
