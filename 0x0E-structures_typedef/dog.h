1a1d651b3d82ab557f41c963eeaa01fafba62487
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-08-01 09:15:11 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Dog definition
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: Dog Definition
+ */
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
