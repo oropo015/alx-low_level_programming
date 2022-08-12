7119ac4263bcd5f176c2998aff4379063b7a6726
Opeyemi <oriolowoopeyemi@yahoo.com>
2022-08-12 14:10:23 -0700

    first commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints list of elements passed to the funcction
+ * @h: A pointer
+ * Return: number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *list;
+	unsigned int i;
+
+	list = h;
+	for (i = 0; list; i++)
+	{
+		printf("[%u] %s\n", list->len, list->str);
+		list = list->next;
+	}
+	return (i);
+}
