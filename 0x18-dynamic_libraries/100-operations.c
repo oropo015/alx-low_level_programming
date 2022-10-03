8ae1f4c63df2860cecdb9bba41c6163da0f2ab37
oropo015 <oriolowoopeyemi@yahoo.com>
2022-10-03 04:06:57 -0700

    Update Lib

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * Perform arithmetic
+ * add - addition
+ * sub - subtraction
+ * mul - Multiplicatio
+ * div2 - Addition
+ * mod - Modulus
+ */
+
+int add(int a, int b)
+{
+	return a + b;
+}
+int sub(int a, int b)
+{
+	return a - b;
+}
+int mul(int a, int b)
+{
+	return a * b;
+}
+int div2(int a, int b)
+{
+	return a / b;
+}
+int mod(int a, int b)
+{
+	return a % b;
+}
